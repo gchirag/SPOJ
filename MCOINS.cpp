0d5c4387cb2419a8ab747c5c5c7eba7d2015f75b
gchirag <guptachirag15@gmail.com>
2014-01-09 18:50:53 +0530

    DP for Tower of coins nim game

--- MCOINS.cpp
+++ MCOINS.cpp
@@ -2,50 +2,28 @@
 #include<cstdlib>
 using namespace std;
 bool arr[1000001];
+
 void build_arr(int k,int l)
 {
 	arr[1]=true;
 	arr[k]=true;
 	arr[l]=true;
-	bool flag1,flag2,flag3;
+
 	bool c1,c2,c3;
+
 	for(int i=2;i<1000001;i++)
 	{
-		flag1 = false;
-		flag2 = false;
-		flag3 = false;
+		c1 = true;
+		c2 = true;
 		
 		if(i!=l && i!=k)
 		{
 			if(i-k>0)
-			{
-				flag1 = true;
 				c1 = arr[i-k];
-			}
 			if(i-l>0)
-			{
-				flag2 = true;
 				c2 = arr[i-l];
-			}
-			
 			c3 = arr[i-1];
-			
-			if(!flag1 && !flag2)
-			{
-				arr[i]=!c3;
-			}
-			if(flag1 && !flag2)
-			{
-				arr[i] = (!c1)||(!c3);
-			}
-			if(!flag1 && flag2)
-			{
-				arr[i] = (!c2)||(!c3);				
-			}
-			if(flag1 && flag2)
-			{
-				arr[i] = (!c1)||(!c2)||(!c3);
-			}
+			arr[i] = (!c1)||(!c2)||(!c3);
 		}
 	}	
 }
@@ -53,11 +31,13 @@ int main()
 {
 	int k,l,m,p;
 	scanf("%d%d%d",&k,&l,&m);
+	//k=read_int();l=read_int();m=read_int();
 	
 	build_arr(k,l);
 	while(m--)
 	{
 		scanf("%d",&p);
+		//p=read_int();
 		if(arr[p])
 			printf("A");
 		else
